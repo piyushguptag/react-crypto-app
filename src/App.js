c0cb6399eeb0cee1abfbbe25819acca58c57391c
piyushguptag <piyushguptagg83@gmail.com>
2023-07-02 18:25:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './component/Header';
+import Home from './component/Home';
+import Coins from './component/Coins';
+import CoinDetails from './component/CoinDetails';
+import Exchanges from './component/Exchanges';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+        <Router>
+        <Header />
+          <Routes>
+            <Route path='/' element ={<Home/>}/>
+            <Route path='/coins' element ={<Coins/>}/>
+            <Route path='/exchanges' element ={<Exchanges/>}/>
+            <Route path='/coins/:id' element ={<CoinDetails/>}/>
+          </Routes>
+        <Footer />
+        </Router>
+  );
+}
+
+export default App;
